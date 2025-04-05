5c2e0873949ea2744d906d505d93b61421b34de3
Valentin <lothvalentin@gmail.com>
2025-04-05 16:43:59 +0200

    connect wallet

--- three_js_clock/vite.config.js
+++ three_js_clock/vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 export default {
-    server: {
-      open: true
-    }
-  };
+  server: {
+    host: '0.0.0.0', // Écoute sur toutes les interfaces réseau
+    port: 5173       // Port par défaut, vous pouvez le changer si nécessaire
+  }
+}
   
