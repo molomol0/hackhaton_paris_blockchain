37b20b738ab14664c81f53aed5f58e99085993f8
Bilel Moudache <bmoudach@student.42.fr>
2025-04-06 14:16:18 +0200

    nice

--- three_js_clock/vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-export default {
-  server: {
-    host: '0.0.0.0', // Écoute sur toutes les interfaces réseau
-    port: 5173       // Port par défaut, vous pouvez le changer si nécessaire
-  }
-}
-  
